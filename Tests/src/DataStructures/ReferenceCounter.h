15f203cdccb5dc6664b8e5b64677c55a803cd5eb
Will Griffiths <wfgriffiths@gmail.com>
2021-11-28 06:40:55 -0800

    Add ReferenceCounter class to aid in testing

---
+++ Tests/src/DataStructures/ReferenceCounter.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <CppUnitTestLogger.h>
+
+namespace Test = Microsoft::VisualStudio::CppUnitTestFramework;
+
+class ReferenceCounter {
+public:
+	ReferenceCounter(int* ptr) : countPtr(ptr) {
+		(*countPtr)++;
+
+		std::ostringstream message;
+		message << "[Constructor] Count = " << *countPtr;
+		Test::Logger::WriteMessage(message.str().c_str());
+	}
+	~ReferenceCounter() {
+		if (!countPtr) {
+			return;
+		}
+			
+		(*countPtr)--;
+
+		std::ostringstream message;
+		message << "[Destructor] Count = " << *countPtr;
+		Test::Logger::WriteMessage(message.str().c_str());
+	}
+
+	ReferenceCounter(ReferenceCounter& other) {
+		this->countPtr = other.countPtr;
+		other.countPtr = nullptr;
+	}
+	ReferenceCounter(ReferenceCounter&& other) {
+		this->countPtr = other.countPtr;
+		other.countPtr = nullptr;
+	}
+
+	ReferenceCounter& operator= (ReferenceCounter& other) {
+		this->countPtr = other.countPtr;
+		other.countPtr = nullptr;
+		return *this;
+	}
+	ReferenceCounter& operator= (ReferenceCounter&& other) {
+		this->countPtr = other.countPtr;
+		other.countPtr = nullptr;
+		return *this;
+	}
+
+private:
+	int* countPtr;
+};
