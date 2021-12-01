dae21bde48b2a6128c05efa99ad7d4e9e5ac613f
Will Griffiths <wfgriffiths@gmail.com>
2021-12-01 08:23:22 -0800

    Fix const for LinkedList

--- DataStructures/src/LinkedList.h
+++ DataStructures/src/LinkedList.h
@@ -18,13 +18,13 @@ namespace ds {
 			}
 		}
 
-		void Insert(T& value) {
+		void Insert(const T& value) {
 			Node* newNode = new Node(value);
 			newNode->next = first;
 			first = newNode;
 		}
 		void Insert(T&& value) {
-			Node* newNode = new Node(std::move(value));
+			Node*const newNode = new Node(std::move(value));
 			newNode->next = first;
 			first = newNode;
 		}
@@ -32,7 +32,7 @@ namespace ds {
 	private:
 
 		struct Node {
-			Node(T& val) : value(val) {}
+			Node(const T& val) : value(val) {}
 			Node(T&& val) : value(std::move(val)) {}
 
 			Node* next = nullptr;
@@ -61,20 +61,20 @@ namespace ds {
 				return iter;
 			}
 
-			const bool operator==(const Iterator& other) const {
+			bool operator==(const Iterator& other) const {
 				return current == other.current;
 			}
-			const bool operator!=(const Iterator& other) const {
+			bool operator!=(const Iterator& other) const {
 				return !operator==(other);
 			}
 		private:
 			Node* current;
 		};
 
-		Iterator begin() {
+		Iterator begin() const {
 			return Iterator(first);
 		}
-		Iterator end() {
+		Iterator end() const {
 			return Iterator(nullptr);
 		}
 	};
