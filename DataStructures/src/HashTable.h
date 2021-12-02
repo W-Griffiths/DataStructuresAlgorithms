20706711d9cabb82dd49d6bde577e9f4b4c7ff28
Will Griffiths <wfgriffiths@gmail.com>
2021-12-02 08:06:48 -0800

    Update HashTable to work with KeyValue pairs

--- DataStructures/src/HashTable.h
+++ DataStructures/src/HashTable.h
@@ -4,30 +4,46 @@
 #include <memory>
 
 namespace ds {
-	template <typename T>
+	template <typename T, typename SearchKey=T>
 	class HashTable {
 	public:
 
 		using listType = LinkedList<T>;
-		HashTable() : arraySize(7), array(new listType[arraySize]), numEntries(0), hasher() {}
+
+		HashTable(std::function<const SearchKey&(const T&)> GetKey = [](const T& t) -> const SearchKey& {return t;})
+			: arraySize(7), array(new listType[arraySize]), numEntries(0), hasher(), GetSearchKey(GetKey) { }
 
 		virtual ~HashTable() {
 			delete[] array;
 		}
 
 		virtual void Add(const T& value) {
-			size_t index = GetIndex(value);
+			const SearchKey& key = GetSearchKey(value);
+			size_t index = GetIndex(key);
 			array[index].Insert(value);
 
 			numEntries++;
 			CheckForTableResize();
 		}
 
-		bool Contains(const T& value) const {
-			auto& [result, _] = Search(value);
+		bool Contains(const SearchKey& searchKey) const {
+			auto& [result, _] = Search(searchKey);
 			return result;
 		}
 
+	protected:
+
+		const std::pair<bool, T*> Search(const SearchKey& lookup) const {
+			size_t index = GetIndex(lookup);
+			auto& listToSearch = array[index];
+			for (T& item : listToSearch) {
+				if (GetSearchKey(item) == lookup) {
+					return { true, &item };
+				}
+			}
+			return { false, nullptr };
+		}
+	
 	private:
 		size_t arraySize;
 		listType* array;
@@ -35,24 +51,14 @@ namespace ds {
 		size_t numEntries;
 		const float maxLoadFactor = 0.8f;
 
-		const std::hash<T> hasher;
+		const std::hash<SearchKey> hasher;
+		std::function<const SearchKey& (const T&)> GetSearchKey;
 
-		const std::pair<bool, const T*> Search(const T& value) const {
-			size_t index = GetIndex(value);
-			auto& listToSearch = array[index];
-			for (const T& item : listToSearch) {
-				if (item == value) {
-					return { true, &value };
-				}
-			}
-			return { false, nullptr };
-		}
-
-		const size_t Hash(const T& value) const {
+		const size_t Hash(const SearchKey& value) const {
 			return hasher(value);
 		}
 
-		const size_t GetIndex(const T& value) const {
+		const size_t GetIndex(const SearchKey& value) const {
 			return Hash(value) % arraySize;
 		}
 
@@ -137,24 +143,24 @@ namespace ds {
 
 	};
 
-	template<typename T>
-	void HashTable<T>::CheckForTableResize() {
+	template<typename T, typename HashKey>
+	void HashTable<T, HashKey>::CheckForTableResize() {
 		const float loadFactor = static_cast<float>(numEntries) / static_cast<float>(arraySize);
 		if (loadFactor > maxLoadFactor) {
 			IncreaseTableSize();
 		}
 		
 	}
-	template<typename T>
-	void HashTable<T>::IncreaseTableSize() {
+	template<typename T, typename HashKey>
+	void HashTable<T, HashKey>::IncreaseTableSize() {
 		const size_t oldSize = arraySize;
 		// TODO - lookup next prime after doubling?
 		arraySize = (arraySize * 2) + 1; // Guarantees odd number
 
 		Rehash(oldSize);
 	}
-	template<typename T>
-	void HashTable<T>::Rehash(const size_t oldSize) {
+	template<typename T, typename HashKey>
+	void HashTable<T, HashKey>::Rehash(const size_t oldSize) {
 		listType* oldArray = array;
 		array = new listType[arraySize];
 
