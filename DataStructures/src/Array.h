e1506e844a17203353945e4a322d6e5b7ba9d185
Will Griffiths <wfgriffiths@gmail.com>
2021-12-17 19:02:10 -0800

    Refactor Array to use RandomAccessIterator

--- DataStructures/src/Array.h
+++ DataStructures/src/Array.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Iterators/RandomAccessIterator.h"
 #include <memory>
 
 namespace ds {
@@ -49,49 +50,7 @@ namespace ds {
 
 	public:
 
-		class Iterator {
-		public:
-			Iterator(T* address) : positionPtr(address) { }
-
-			T& operator*() {
-				return *positionPtr;
-			}
-
-			Iterator& operator++() {
-				positionPtr++;
-				return *this;
-			}
-			Iterator operator++(int) {
-				Iterator iter(positionPtr);
-				++(*this);
-				return iter;
-			}
-			Iterator& operator--() {
-				positionPtr--;
-				return *this;
-			}
-			Iterator operator--(int) {
-				Iterator iter(positionPtr);
-				--(*this);
-				return iter;
-			}
-
-			Iterator operator+(int length) {
-				return Iterator(positionPtr + length);
-			}
-			Iterator operator-(int length) {
-				return Iterator(positionPtr - length);
-			}
-
-			const bool operator==(const Iterator& other) const {
-				return positionPtr == other.positionPtr;
-			}
-			const bool operator!=(const Iterator& other) const {
-				return !operator==(other);
-			}
-		private:
-			T* positionPtr;
-		};
+		using Iterator = RandomAccessIterator<T>;
 
 		Iterator begin() {
 			return Iterator(array.get());
