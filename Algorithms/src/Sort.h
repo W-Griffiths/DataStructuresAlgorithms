e914e0dee9a2aba462e230ff3c94ff6e4b24a701
Will Griffiths <wfgriffiths@gmail.com>
2021-12-25 01:17:29 -0800

    Add Quicksort algorithm

--- Algorithms/src/Sort.h
+++ Algorithms/src/Sort.h
@@ -1,7 +1,55 @@
 #pragma once
 #include "Array.h"
+#include "Utilities.h"
+#include "Iterators/RandomAccessIterator.h"
 
 namespace alg {
 
+	template<typename T>
+	using RAI = RandomAccessIterator<T>;
+
+	template<typename T>
+	void Quicksort(RAI<T> begin, RAI<T> end) {
+		util::Shuffle(begin, end);
+
+		quicksort(begin, end);
+	}
+
+	template<typename T>
+	void Quicksort(const ds::Array<T>& array) {
+		Quicksort(array.begin(), array.end());
+	}
+
+	namespace {
+
+		template<typename T>
+		void quicksort(RAI<T> left, RAI<T> right) {
+			if (left >= right) return;
+
+			RAI<T> pivot = Partition(left, right);
+			quicksort(left, pivot);
+			quicksort(pivot+1, right);
+		}
+
+		template<typename T>
+		RAI<T> Partition(RAI<T> left, RAI<T> right) {
+			RAI<T> pivot = left;
+
+			while (left < right) {
+				while (*(++left) < *pivot) {
+					if (left == right) break;
+				}
+				while (*(--right) > *pivot) {
+					if (right == left) break;
+				}
+				if (left >= right) break;
+				util::Swap(*left, *right);
+			}
+			--left;
+			util::Swap(*pivot, *left);
+			return left;
+		}
+
+	}
 
 }
--- Tests/src/Algorithms/SortTests.cpp
+++ Tests/src/Algorithms/SortTests.cpp
@@ -1,12 +1,73 @@
 #include "CppUnitTest.h"
 #include "Sort.h"
-#include "Array.h"
 #include "Utilities.h"
+#include "Array.h"
+#include "List.h"
 #include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Sort {
 
+	TEST_CLASS(Quicksort)
+	{
+	public:
+
+		TEST_METHOD(SortNumbers)
+		{
+			const int length = 1000;
+			ds::Array<int> arrayA(length);
+			ds::Array<int> arrayB(length);
+
+			for (int i = 0; i < length; i++)
+			{
+				arrayA[i] = i;
+				arrayB[i] = i;
+			}
+
+			Assert::IsTrue(arrayA == arrayB);
+
+			util::Shuffle(arrayB);
+			Assert::IsTrue(arrayA != arrayB);
+
+			alg::Quicksort(arrayB);
+
+			Assert::IsTrue(arrayA == arrayB);
+		}
+
+		TEST_METHOD(SortStrings)
+		{
+			ds::List<std::string> words;
+			words.Add("Beta");
+			words.Add("Foxtrot");
+			words.Add("Alpha");
+			words.Add("Charlie");
+			words.Add("Echo");
+			words.Add("Delta");
+
+			alg::Quicksort(words.begin(), words.end());
+
+			Assert::AreEqual(std::string("Alpha"), words[0]);
+			Assert::AreEqual(std::string("Beta"), words[1]);
+			Assert::AreEqual(std::string("Charlie"), words[2]);
+			Assert::AreEqual(std::string("Delta"), words[3]);
+			Assert::AreEqual(std::string("Echo"), words[4]);
+			Assert::AreEqual(std::string("Foxtrot"), words[5]);
+		}
+
+		TEST_METHOD(SortChars)
+		{
+			const int length = 26;
+			ds::Array<char> keyboard("qwertyuiopasdfghjklzxcvbnm", length);
+			ds::Array<char> alphabet("abcdefghijklmnopqrstuvwxyz", length);
+
+			Assert::IsTrue(keyboard != alphabet);
+
+			alg::Quicksort(keyboard);
+
+			Assert::IsTrue(keyboard == alphabet);
+		}
+
+	};
 
 }
