5de93707a1bb3d26db8c0dce08120fa5de79139b
Will Griffiths <wfgriffiths@gmail.com>
2021-12-29 01:25:17 -0800

    Add ShiftElements function

--- Algorithms/src/Utilities.h
+++ Algorithms/src/Utilities.h
@@ -20,6 +20,9 @@ namespace util {
 	template<typename T>
 	void Swap(T& A, T& B);
 
+	template<typename T>
+	void ShiftElements(RAI<T> begin, RAI<T> end, const ptrdiff_t amount);
+
 	size_t StringLength(const char*);
 	std::string ToString(const ds::Array<char>);
 	//-----
@@ -50,6 +53,35 @@ namespace util {
 		T temp = A;
 		A = B;
 		B = temp;
+	}
+
+	/// <summary>
+	/// Moves the input sequence by the desired amount.
+	/// Warning: This overwrites elements at the destination.
+	/// </summary>
+	template<typename T>
+	void ShiftElements(RAI<T> begin, RAI<T> end, const ptrdiff_t amount) {
+		ptrdiff_t length = end - begin;
+
+		if (length == 0 || amount == 0) return;
+
+		#ifdef _DEBUG
+		if (length < 0) {
+			throw std::invalid_argument("Element sequence length is negative.");
+		}
+		#endif
+
+		if (amount > 0) {
+			// Shift right
+			for (ptrdiff_t i = length - 1; i >= 0; i--) {
+				begin[i + amount] = begin[i];
+			}
+		} else{
+			// Shift left
+			for (ptrdiff_t i = 0; i < length; i++) {
+				begin[i + amount] = begin[i];
+			}
+		}
 	}
 
 }
