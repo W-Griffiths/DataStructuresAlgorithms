cb9a42263534ce3a087794b00ea2a085f1efe368
Will Griffiths <wfgriffiths@gmail.com>
2022-01-03 00:00:45 -0800

    Add IsAlphabetic and IsAlphaNumeric char utility functions

--- Utilities/src/StringUtilities.cpp
+++ Utilities/src/StringUtilities.cpp
@@ -7,3 +7,11 @@ size_t CStringLength(const char* cString) {
 	}
 	return len;
 }
+
+bool IsAlphabetic(const char c) {
+	return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == ' ');
+}
+
+bool IsAlphaNumeric(const char c) {
+	return IsAlphabetic(c) || (c >= '0' && c <= '9');
+}
--- Utilities/src/StringUtilities.h
+++ Utilities/src/StringUtilities.h
@@ -1,3 +1,6 @@
 #pragma once
 
 size_t CStringLength(const char*);
+
+bool IsAlphabetic(const char);
+bool IsAlphaNumeric(const char);
